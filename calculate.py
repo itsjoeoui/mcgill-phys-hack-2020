6dc85b38fc5974ad02492e70d7bd6913824d365a
itsjoeoui <xiaowei.yu@dawsoncollege.qc.ca>
2020-11-13 15:00:55 -0500

    Further clean up the data and add another data set
    Signed-off-by: itsjoeoui <xiaowei.yu@dawsoncollege.qc.ca>

--- calculate.py
+++ calculate.py
@@ -1,4 +1,5 @@
 import pandas
+import statistics
 
 def get_data(file_name, clean_up=False, divide=0):
 
@@ -6,7 +7,27 @@ def get_data(file_name, clean_up=False, divide=0):
         # Returns the index of the coordinate in the list
         if x in range(0, side_length) and y in range(0, side_length):
             return x * side_length + y
+        # print(x, y, side_length)
         raise ValueError("Position out of range!")
+
+    def get_coordinate(pos, side_length):
+        if pos < side_length**2:
+            return (int(pos/side_length), pos % side_length)
+        # print(pos, side_length)
+        raise ValueError("Position out of range!")
+
+    def get_adjacent(x, y, side_length):
+        # Returns all adjacent values of the coordinate passed in
+        all_adjacent = []
+        if x >= 1:
+            all_adjacent.append(get_position(x - 1, y, side_length))
+        if y >= 1:
+            all_adjacent.append(get_position(x, y - 1, side_length))
+        if y <= side_length - 2:
+            all_adjacent.append(get_position(x, y + 1, side_length))
+        if x <= side_length - 2:
+            all_adjacent.append(get_position(x + 1, y, side_length))
+        return all_adjacent
 
     df = pandas.read_csv(file_name)
 
@@ -29,6 +50,14 @@ def get_data(file_name, clean_up=False, divide=0):
                 for y in range(coor[1], coor[1] + divide):
                     values.append(output[get_position(x, y, side_length)])
             output_fixed.append(max(values))
+        while 0 in output_fixed:
+            for i in range(len(output_fixed)):
+                if output_fixed[i] == 0:
+                    x, y = get_coordinate(i, 13)
+                    adjacent = get_adjacent(x, y, 13)
+                    values = [output_fixed[i] for i in adjacent if output_fixed[i] != 0]
+                    if values != []:
+                        output_fixed[i] = statistics.mean(values)
         return output_fixed
     else:
         return output
